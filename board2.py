6e6f522be4ad58e60a7e11d3b83ad7a89f1c7311
Kacper Buczko <buczko.kacper2@gmail.com>
2021-06-28 21:47:39 +0200

    Game mechanics update

--- board2.py
+++ board2.py
@@ -1,7 +1,9 @@
 import pygame
 import math
-
+from operator import itemgetter
 pygame.init()
+
+#DODAC ZLICZANIE FIGUR, DODAC DOWOLNE PORUSZANIE GDY POLA SĄSIADUJĄCE SĄ ZAJĘTE
 
 # Screen
 WIDTH = 600
@@ -19,16 +21,14 @@ white_IMAGE = pygame.transform.scale(pygame.image.load("images/pionek_bialyy.jpg
 black_IMAGE = pygame.transform.scale(pygame.image.load("images/pionek_czarnyy.jpg"), (55, 55))
 
 # Fonts
-END_FONT = pygame.font.SysFont('arial', 40)
+END_FONT = pygame.font.SysFont('arial', 32)
 
 images = []
 draw = False
 
 run = True
 
-next_move = []
-
-
+      
 
 white_turn = True
 black_turn = False
@@ -78,7 +78,11 @@ class board(object):
     def render(self):
         win.fill(ORANGE)
         self.draw_grid()
-
+        self.display_message("Tura gracza:", 615, 100)
+        if(white_turn):
+            self.display_message("Biały", 660, 160)
+        else:
+            self.display_message("Czarny", 650, 160)
         # Drawing figures
         for image in images:
             x, y, IMAGE = image
@@ -86,6 +90,11 @@ class board(object):
 
         pygame.display.update()
 
+    def display_message(self,content,x,y):
+        #win.fill(WHITE)
+        text = END_FONT.render(content, 1, BLACK)
+        win.blit(text, (x, y))
+        pygame.display.update()
 
     def player_move(self, game_array):
         global white_turn, black_turn, images
@@ -100,36 +109,179 @@ class board(object):
                 
                 # Distance between mouse and the centre of the square
                 dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
-
-                
+                 
                 # If it's inside the square
                 if dis < WIDTH // ROWS // 2 and can_play:
                     if white_turn:  # white turn
                         if self.white_pawns == 50:
-                            images.append((x, y, white_IMAGE))
+                            images.append([x, y, white_IMAGE])
                             white_turn = False
                             black_turn = True
                             game_array[i][j] = (x, y, 'w', False)
                             self.white_pawns = self.white_pawns - 1
+                            if(y == 570):
+                                if(x == 30):
+                                    self.next_moveb=[[x,y-60],[x+60,y], [x+60, y-60]]
+                                elif(x == 570):
+                                    self.next_moveb=[[x,y+60],[x+60,y], [x+60, y+60]]
+                                else:
+                                    self.next_moveb = [[x-60,y],[x+60,y],[x-60,y-60],[x+60,y-60],[x,y-60]]
+                            elif(y == 30):
+                                if(x == 30):
+                                    self.next_moveb = [[x, y+60],[x+60,y],[x+60,y+60]]
+                                elif (x == 570):
+                                    self.next_moveb = [[x, y+60],[x-60, y], [x-60,y+60]]
+                                else:
+                                    self.next_moveb = [[x-60,y],[x+60,y],[x-60,y+60],[x+60,y+60],[x,y+60]]
+                            else:
+                                if(x==30):
+                                    self.next_moveb = [[x,y+60],[x,y-60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                                elif(x==570):
+                                    self.next_moveb= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60]]
+                                else:
+                                    self.next_moveb= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+
                         elif [x,y] in self.next_move:
                             images.append((x, y, white_IMAGE))
                             white_turn = False
                             black_turn = True
                             game_array[i][j] = (x, y, 'w', False)
                             self.white_pawns = self.white_pawns - 1
-                        self.next_moveb = [[x-60,y-60],[x,y-60],[x+60,y],[x,y+60],[x+60,y+60],[x-60,y],[x-60,y+60],[x+60,y-60]]  
-                            
-                    elif black_turn and [x,y] in self.next_moveb:  # black turn
-                        images.append((x, y, black_IMAGE))
-                        white_turn = True
-                        black_turn = False
-                        game_array[i][j] = (x, y, 'b', False)
-                        self.black_pawns = self.black_pawns- 1
-                        self.a = x+60
-                        self.b = y+60
-                        self.next_move = [[x-60,y-60],[x,y-60],[x+60,y],[x,y+60],[x+60,y+60],[x-60,y],[x-60,y+60],[x+60,y-60]]
+                            if(y == 570):
+                                if(x == 30):
+                                    self.next_moveb=[[x,y-60],[x+60,y], [x+60, y-60]]
+                                elif(x == 570):
+                                    self.next_moveb=[[x,y+60],[x-60,y], [x-60, y+60]]
+                                else:
+                                    self.next_moveb = [[x-60,y],[x+60,y],[x-60,y-60],[x+60,y-60],[x,y-60]]
+                            elif(y == 30):
+                                if(x == 30):
+                                    self.next_moveb = [[x, y+60],[x+60,y],[x+60,y+60]]
+                                elif (x == 570):
+                                    self.next_moveb = [[x, y+60],[x-60, y], [x-60,y+60]]
+                                else:
+                                    self.next_moveb = [[x-60,y],[x+60,y],[x-60,y+60],[x+60,y+60],[x,y+60]]
+                            else:
+                                if(x==30):
+                                    self.next_moveb = [[x,y+60],[x,y-60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                                elif(x==570):
+                                    self.next_moveb= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60]]
+                                else:
+                                    self.next_moveb= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                        else:
+                            images.append((x, y, white_IMAGE))
+                            white_turn = False
+                            black_turn = True
+                            game_array[i][j] = (x, y, 'w', False)
+                            self.white_pawns = self.white_pawns - 1
+                            if(y == 570):
+                                if(x == 30):
+                                    self.next_moveb=[[x,y-60],[x+60,y], [x+60, y-60]]
+                                elif(x == 570):
+                                    self.next_moveb=[[x,y+60],[x-60,y], [x-60, y+60]]
+                                else:
+                                    self.next_moveb = [[x-60,y],[x+60,y],[x-60,y-60],[x+60,y-60],[x,y-60]]
+                            elif(y == 30):
+                                if(x == 30):
+                                    self.next_moveb = [[x, y+60],[x+60,y],[x+60,y+60]]
+                                elif (x == 570):
+                                    self.next_moveb = [[x, y+60],[x-60, y], [x-60,y+60]]
+                                else:
+                                    self.next_moveb = [[x-60,y],[x+60,y],[x-60,y+60],[x+60,y+60],[x,y+60]]
+                            else:
+                                if(x==30):
+                                    self.next_moveb = [[x,y+60],[x,y-60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                                elif(x==570):
+                                    self.next_moveb= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60]]
+                                else:
+                                    self.next_moveb= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                                   
+                        a = [[item[0], item[1]] for item in images]
+                        temp = [element for element in self.next_moveb if element in a]
+                        
+                        for i in range(len(temp)):
+                            self.next_moveb.remove(temp[i])
+                        
+                        print("Biały: ",x,y)
+                        print("Ruchy: ", self.next_moveb)
+                        
 
-                   
+                    elif black_turn:  # black turn
+                        if([x,y] in self.next_moveb):  
+                            images.append([x, y, black_IMAGE])
+                            white_turn = True
+                            black_turn = False
+                            game_array[i][j] = (x, y, 'b', False)
+                            self.black_pawns = self.black_pawns- 1
+                            self.a = x+60
+                            self.b = y+60
+                            if(y == 570):
+                                if(x == 30):
+                                    self.next_move=[[x,y-60],[x-60,y], [x-60, y+60]]
+                                elif(x == 570):
+                                    self.next_move=[[x,y-60],[x-60,y], [x-60, y-60]]
+                                else:
+                                    self.next_move = [[x-60,y],[x+60,y],[x-60,y-60],[x+60,y-60],[x,y-60]]
+                            elif(y == 30):
+                                if(x == 30):
+                                    self.next_move = [[x, y+60],[x+60,y],[x+60,y+60]]
+                                elif (x == 570):
+                                    self.next_move = [[x, y+60],[x-60, y], [x-60,y+60]]
+                                else:
+                                    self.next_move = [[x-60,y],[x+60,y],[x-60,y+60],[x+60,y+60],[x,y+60]]
+                            else:
+                                if(x==30):
+                                    self.next_move = [[x,y+60],[x,y-60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                                elif(x==570):
+                                    self.next_move= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60]]
+                                else:
+                                    self.next_move= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                        else:
+                            images.append([x, y, black_IMAGE])
+                            white_turn = True
+                            black_turn = False
+                            game_array[i][j] = (x, y, 'b', False)
+                            self.black_pawns = self.black_pawns- 1
+                            self.a = x+60
+                            self.b = y+60
+                            if(y == 570):
+                                if(x == 30):
+                                    self.next_move=[[x,y-60],[x-60,y], [x-60, y+60]]
+                                elif(x == 570):
+                                    self.next_move=[[x,y-60],[x-60,y], [x-60, y-60]]
+                                else:
+                                    self.next_move = [[x-60,y],[x+60,y],[x-60,y-60],[x+60,y-60],[x,y-60]]
+                            elif(y == 30):
+                                if(x == 30):
+                                    self.next_move = [[x, y+60],[x+60,y],[x+60,y+60]]
+                                elif (x == 570):
+                                    self.next_move = [[x, y+60],[x-60, y], [x-60,y+60]]
+                                else:
+                                    self.next_move = [[x-60,y],[x+60,y],[x-60,y+60],[x+60,y+60],[x,y+60]]
+                            else:
+                                if(x==30):
+                                    self.next_move = [[x,y+60],[x,y-60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                                elif(x==570):
+                                    self.next_move= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60]]
+                                else:
+                                    self.next_move= [[x,y+60],[x,y-60],[x-60,y],[x-60,y-60],[x-60,y+60],[x+60,y],[x+60,y-60],[x+60,y+60]]
+                            
+                        a = [[item[0], item[1]] for item in images]
+                        print("CZARNY: ",x,y)
+                        
+                       
+
+                        temp = [element for element in self.next_move if element in a]
+                        
+                        for i in range(len(temp)):
+                            self.next_move.remove(temp[i])
+
+                        print("Ruchy: ", self.next_move)
+                        
+
+                       
+                        
+                                
 
 
                 
